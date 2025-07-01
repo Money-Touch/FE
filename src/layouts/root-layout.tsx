a9bdd6e61da2a5936dcbd32014dcb1dae765a071
gpdnjs8 <jade3735@naver.com>
2025-07-01 21:33:12 +0900

    🐛 Fix: footer, root-layout 수정

--- src/layouts/root-layout.tsx
+++ src/layouts/root-layout.tsx
@@ -2,18 +2,44 @@ import { Outlet, useLocation } from "react-router-dom";
 import Footer from "../components/footer/footer";
 
 const RootLayout = () => {
-    const location = useLocation();
-    const currentPath = location.pathname;
+  const location = useLocation();
+  const currentPath = location.pathname;
 
-    const showFooterPaths = ["/home", "/money", "/feed", "/mypage"];
-    const shouldShowFooter = showFooterPaths.some(path => currentPath.startsWith(path));
+  const showFooterPaths = ["/home", "/money", "/feed", "/mypage"];
+  const shouldShowFooter = showFooterPaths.some((path) =>
+    currentPath.startsWith(path)
+  );
 
-    return (
-        <>
-            <Outlet />
-            {shouldShowFooter && <Footer />}
-        </>
-    );
+  return (
+    <div
+      className="pageContainer"
+      style={{
+        position: "relative",
+      }}
+    >
+      <div
+        style={{
+          height: "100%",
+          overflowY: "auto",
+        }}
+      >
+        <Outlet />
+      </div>
+
+      {shouldShowFooter && (
+        <div
+          style={{
+            position: "fixed",
+            bottom: 0,
+            left: 0,
+            width: "100%",
+          }}
+        >
+          <Footer />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default RootLayout;
