43dd06de62188df98c9957c807a849d438b541e6
gpdnjs8 <jade3735@naver.com>
2025-07-07 21:34:22 +0900

    🎨 Design: 내 순위 디자인 변경

--- src/components/home/ranking/myRank.tsx
+++ src/components/home/ranking/myRank.tsx
@@ -12,10 +12,7 @@ function MyRank({ user }: MyRankProps) {
       <S.RankBox>
         <S.ProfileImg src={user.profileImage || profile_t} alt="profile" />
         <S.ProfileDes>
-          {user.name}님의 순위는{" "}
-          <strong>
-            {user.currentRank}위({user.wiseCount}회)
-          </strong>{" "}
+          {user.name}님의 순위는 {user.currentRank}위({user.wiseCount}회){" "}
           입니다.
         </S.ProfileDes>
       </S.RankBox>
