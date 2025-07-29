869a24ca727a763228298b797a790fe7c5638d8e
양인서 <113487989+sheepyis@users.noreply.github.com>
2025-07-29 14:30:04 +0900

    ✨ Feat: 마이페이지 통신 1차 연결

--- src/pages/auth/mypage/mypage.tsx
+++ src/pages/auth/mypage/mypage.tsx
@@ -3,8 +3,12 @@ import * as M from '../../../styles/auth/mypage/mypage.style';
 import Profile from '../../../components/auth/mypage/mypage/profile';
 import ListButton from '../../../components/auth/mypage/mypage/button/list-button';
 import ListSection from '../../../components/auth/mypage/mypage/section/list-section';
+// import { useMypageQuery } from '../../../hooks/auth/mypage/useMypageQuery';
 
 const Mypage = () => {
+  // const { data } = useMypageQuery();
+  // console.log(data);
+
   return (
     <div
       className={`${S.AgreeContainer} !pb-[11.6rem] !bg-[var(--color-B2)] !px-0`}
