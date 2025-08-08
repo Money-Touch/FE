d0d14ea1b999dadfbb57d505ea566f5e540f1938
양인서 <113487989+sheepyis@users.noreply.github.com>
2025-08-09 01:03:39 +0900

    🎨 Design: money 페이지 UI 2차 개선

--- src/styles/budget/money.styles.ts
+++ src/styles/budget/money.styles.ts
@@ -53,7 +53,7 @@ export const ArrowBtn = styled.img<{ disabled?: boolean }>`
   width: 2.4rem;
   height: 2.4rem;
   cursor: pointer;
-  // opacity: ${({ disabled }) => (disabled ? 0.3 : 1)};
+  opacity: ${({ disabled }) => (disabled ? 0.3 : 1)};
   pointer-events: ${({ disabled }) => (disabled ? 'none' : 'auto')};
 `;
 
