36416570c70b5626e1baef9348781ca69dd59892
양인서 <113487989+sheepyis@users.noreply.github.com>
2025-08-14 20:49:41 +0900

    🎨 Design: 회원가입 페이지 스타일링 수정

--- src/styles/auth/login/login.style.ts
+++ src/styles/auth/login/login.style.ts
@@ -1,5 +1,6 @@
 // login.tsx
-export const LoginContainer = 'bg-[var(--color-B1)] flex flex-col items-center';
+export const LoginContainer =
+  'bg-[var(--color-B1)] flex flex-col items-center px-[2.4rem]';
 
 export const LogoContainer = 'flex items-end mt-[12.3rem]';
 
@@ -8,9 +9,9 @@ export const LogoImg = 'w-[6rem] h-[6rem]';
 export const LogoP = 'text-[3rem] font-hakgyoB text-[var(--color-mainColor1)]';
 
 // loginForm.tsx
-export const LoginFormContainer = 'w-[37.7rem] flex flex-col gap-[1.6rem]';
+export const LoginFormContainer = 'w-full flex flex-col gap-[1.6rem]';
 
-export const InputContainer = 'flex flex-col gap-[0.6rem] mt-[5rem]';
+export const InputContainer = 'w-full flex flex-col gap-[0.6rem] mt-[5rem]';
 
 export const LoginButton =
   'w-full h-[4.5rem] rounded-[0.5rem] cursor-pointer bg-[var(--color-G1)] text-[var(--color-white)] text-[1.5rem] font-medium';
@@ -25,7 +26,8 @@ export const IconImg =
   'absolute right-[1.5rem] top-1/2 translate-y-[-50%] w-[1.8rem] h-[1.8rem] cursor-pointer';
 
 // list-menu.tsx
-export const ListMenuContainer = 'flex gap-[2.4rem] mt-[1.6rem] mb-[3rem]';
+export const ListMenuContainer =
+  'w-full items-center justify-center flex gap-[2.4rem] mt-[1.6rem] mb-[3rem]';
 
 // item-menu.tsx
 export const ItemP = (clickable: boolean) =>
@@ -33,7 +35,7 @@ export const ItemP = (clickable: boolean) =>
 
 // kakao.tsx
 export const KakaoButton =
-  'w-[37.7rem] h-[4.5rem] cursor-pointer bg-[var(--color-yellow)] px-[1rem] rounded-[0.5rem]';
+  'w-full h-[4.5rem] cursor-pointer bg-[var(--color-yellow)] px-[1rem] rounded-[0.5rem]';
 
 export const InnerContainer =
   'flex items-center justify-center gap-[0.8rem] text-[1.5rem] font-medium text-[rgba(0,0,0,0.85)]';
