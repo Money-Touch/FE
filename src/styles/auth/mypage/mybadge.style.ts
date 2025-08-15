d8f737892194cef5f0ed1034bff34229dc1d4e73
zo0o0 <hjoo1207@naver.com>
2025-08-15 12:58:25 +0900

    🐛 Fix: 배지페이지 헤더/푸터 수정

--- src/styles/auth/mypage/mybadge.style.ts
+++ src/styles/auth/mypage/mybadge.style.ts
@@ -1,6 +1,8 @@
-export const container = 'flex flex-col';
+export const container =
+  'flex-col !h-[calc(100vh-13rem)] !mb-[13rem] !overflow-y-auto !pt-[8.4rem]';
 
-export const topContainer = 'flex flex-col mx-[2.4rem]';
+export const topContainer =
+  'flex flex-col w-full mx-auto px-[1.6rem] mx-[2.4rem]';
 
 export const titleContainer = 'mt-[1rem] flex items-center gap-[0.6rem]';
 
@@ -27,7 +29,7 @@ export const badgeName =
   'mt-[1rem] text-[1.2rem] leading-[1.6rem] text-[var(--color-G1)] text-center';
 
 export const selectFooter =
-  'w-full max-w-[425px] h-[130px] bg-white rounded-t-[20px] flex justify-between px-[2.4rem] pt-[1.5rem] shadow-[0_0_2px_0_rgba(0,0,0,0.16)] gap-[2.3rem]';
+  'fixed bottom-0 left-0 w-full h-[13rem] bg-white rounded-t-[20px] flex justify-between px-[2.4rem] pt-[1.5rem] shadow-[0_0_2px_0_rgba(0,0,0,0.16)] gap-[2.3rem] z-50';
 
 export const cancelButton =
   'flex-1 h-[4.7rem] bg-[var(--color-G7)] text-[2rem] text-[var(--color-G4)] rounded-[0.5rem]';
