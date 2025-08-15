9c9a567e1bf25e3229b0ea9dc0df3f8d75222845
gpdnjs8 <jade3735@naver.com>
2025-08-15 14:15:32 +0900

    🎨 Design: header, footer 고정에 따른 페이지 레이아웃 수정

--- src/styles/home/ranking.style.ts
+++ src/styles/home/ranking.style.ts
@@ -1,10 +1,10 @@
 // ranking.tsx
 export const Container =
-  'flex flex-col px-[2.4rem] bg-[linear-gradient(180deg,_#e0fadd_0%,_var(--color-M2)_56.31%)]';
+  'flex flex-col px-[2.4rem] overflow-y-auto overflow-x-hidden bg-[linear-gradient(180deg,_#e0fadd_0%,_var(--color-M2)_56.31%)]';
 
 //myRank.tsx
 export const MyRankBox =
-  'w-full max-w-[37.7rem] h-[5rem] min-h-[5rem] mt-[2.6rem] mx-auto bg-white/80 border border-[#cee5f1] rounded-[1rem] backdrop-blur-[2rem] flex justify-center items-center box-border px-[2.4rem]';
+  'w-full max-w-[37.7rem] h-[5rem] min-h-[5rem] mx-auto bg-white/80 border border-[#cee5f1] rounded-[1rem] flex justify-center items-center box-border px-[2.4rem]';
 export const RankBox = 'flex items-center gap-[1rem]';
 export const ProfileImg = 'w-[3rem] h-[3rem] object-cover rounded-full';
 export const ProfileDes =
@@ -13,24 +13,24 @@ export const ProfileDes =
 // topUser.tsx
 export const Top3Wrapper =
   'flex justify-center items-end gap-[2.2rem] mt-[4.2rem]';
-export const TopUser = 'flex flex-col items-center relative';
+export const TopUser = 'flex flex-col items-center';
 export const ProfileWrapper =
-  'relative flex flex-col items-center w-[5.2rem] h-[7.6rem] mb-[0.4rem]';
-export const Medal = 'w-[2.8rem] h-[2.8rem] absolute';
+  'flex flex-col items-center w-[5.2rem] h-[7.6rem] mb-[0.4rem]';
+export const Medal = 'w-[2.8rem] h-[2.8rem]  mb-[-2.8rem]';
 export const Profile = 'w-[5.2rem] h-[5.2rem] mt-[2.4rem]';
 export const InfoWrapper =
   'flex flex-col items-center justify-center w-[5.2rem] h-[5.2rem]';
 export const UserName =
   'w-[5.2rem] h-[2.4rem] font-medium text-[1.6rem] text-center text-[var(--color-G1)]';
 export const Count =
-  'w-[5.2rem] h-[2.6rem] gap-[1rem] rounded-[10rem] border border-[#cee5f1] px-[0.5rem] bg-white/60 backdrop-blur-[2rem] font-medium text-[1.2rem] text-center text-[var(--color-G3)] flex items-center justify-center';
-export const Podium = 'w-[7rem] h-auto mt-[0.3rem] z-0';
+  'w-[5.2rem] h-[2.6rem] gap-[1rem] rounded-[10rem] border border-[#cee5f1] px-[0.5rem] bg-white/60 font-medium text-[1.2rem] text-center text-[var(--color-G3)] flex items-center justify-center';
+export const Podium = 'w-[7rem] h-auto mt-[0.3rem]';
 
 // otherUser.tsx
 export const OtherListWrapper =
-  'w-[calc(100%+4.8rem)] max-w-[42.5rem] h-[41.8rem] rounded-t-[1.5rem] bg-[var(--color-B1)] shadow-[0_0_1rem_0_#0000000d] box-border px-[2.4rem]';
+  'w-[calc(100%+4.8rem)] h-[41.8rem] rounded-t-[1.5rem] bg-[var(--color-B1)] shadow-[0_0_1rem_0_#0000000d] box-border px-[2.4rem]';
 export const OtherListBox =
-  'w-full max-w-[37.7rem] h-[31.4rem] mt-[2.8rem] gap-[2.2rem] box-border';
+  'w-full h-[31.4rem] mt-[2.8rem] gap-[2.2rem] box-border';
 export const OtherUser =
   'flex items-center justify-between h-[2.6rem] leading-[2.2rem] mb-[2.2rem] text-[var(--color-G1)]';
 export const RankNumber = 'w-[0.9rem] font-medium text-[1.4rem]';
