ffb3b9890adcfe450d177abdc1e7017b0b49e0fa
zo0o0 <hjoo1207@naver.com>
2025-08-15 13:03:49 +0900

    🐛 Fix: 배지페이지 푸터 크기 추가 수정

--- src/styles/auth/mypage/mybadge.style.ts
+++ src/styles/auth/mypage/mybadge.style.ts
@@ -29,7 +29,7 @@ export const badgeName =
   'mt-[1rem] text-[1.2rem] leading-[1.6rem] text-[var(--color-G1)] text-center';
 
 export const selectFooter =
-  'fixed bottom-0 left-0 w-full h-[13rem] bg-white rounded-t-[20px] flex justify-between px-[2.4rem] pt-[1.5rem] shadow-[0_0_2px_0_rgba(0,0,0,0.16)] gap-[2.3rem] z-50';
+  'fixed bottom-0 max-w-[425px] w-full h-[13rem] bg-white rounded-t-[20px] flex justify-between px-[2.4rem] pt-[1.5rem] shadow-[0_0_2px_0_rgba(0,0,0,0.16)] gap-[2.3rem] z-50';
 
 export const cancelButton =
   'flex-1 h-[4.7rem] bg-[var(--color-G7)] text-[2rem] text-[var(--color-G4)] rounded-[0.5rem]';
